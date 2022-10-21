70fb50414d0aa8809026b3817d19d690d0159674
Victor Rodrigues <garevictor@hotmail.com>
2022-10-21 14:29:19 +0100

    Changed appsettings and removed config write

--- src/ExevopanNotification.ApplicationCore/Services/TelegramService.cs
+++ src/ExevopanNotification.ApplicationCore/Services/TelegramService.cs
@@ -15,7 +15,6 @@ namespace ExevopanNotification.ApplicationCore.Services
 
         public TelegramService(IOptions<ApplicationConfig> appConfig)
         {
-            Console.Write(appConfig.Value.ToJson());
             _telegramConfig = appConfig.Value.TelegramConfig;
             _telegramBotClient = new TelegramBotClient(_telegramConfig.Key);
         }
