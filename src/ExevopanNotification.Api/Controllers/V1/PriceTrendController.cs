4df8db0a87435dbbf15ff6cd33057d7e0e9401f1
Victor Rodrigues <garevictor@hotmail.com>
2023-10-06 21:05:57 +0100

    New price trend endpoints (#5)

---
+++ src/ExevopanNotification.Api/Controllers/V1/PriceTrendController.cs
@@ -0,0 +1,48 @@
+﻿using ExevopanNotification.Api.Controllers.Base;
+using ExevopanNotification.ApplicationCore.Interfaces.Services;
+using ExevopanNotification.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExevopanNotification.Api.Controllers.V1
+{
+    public class PriceTrendController : BaseController
+    {
+        private readonly IPriceTrendService _priceTrendService;
+
+        public PriceTrendController(IPriceTrendService priceTrendService)
+        {
+            _priceTrendService = priceTrendService;
+        }
+
+
+        [HttpGet("{characterName}")]
+        public async Task<IActionResult> GetPriceTrend(string characterName, int? analyzeMinLevel = 300, int? analyzeMaxLevel = null)
+        {
+            return await Analyze(characterName, false, analyzeMinLevel, analyzeMaxLevel);
+        }
+
+        [HttpGet("{characterName}/history")]
+        public async Task<IActionResult> GetPriceTrendHistory(string characterName, int? analyzeMinLevel = 300, int? analyzeMaxLevel = null)
+        {
+
+            return await Analyze(characterName, true, analyzeMinLevel, analyzeMaxLevel);
+        }
+
+        private async Task<IActionResult> Analyze(string characterName, bool history, int? analyzeMinLevel, int? analyzeMaxLevel)
+        {
+            var filterLimits = FilterLimits.Create(analyzeMinLevel, analyzeMaxLevel);
+            var priceTrend = await _priceTrendService.Analyze(characterName, history, filterLimits);
+
+            if (priceTrend == 0)
+            {
+                return Problem();
+            }
+
+            return Ok(new
+            {
+                characterName,
+                priceTrend
+            });
+        }
+    }
+}
--- src/ExevopanNotification.Domain/Entities/FilterLimits.cs
+++ src/ExevopanNotification.Domain/Entities/FilterLimits.cs
@@ -10,5 +10,10 @@
 
         public int MinLevel { get; private set; }
         public int MaxLevel { get; private set; }
+
+        public static FilterLimits Create(int? minLevel, int? maxLevel)
+        {
+            return new FilterLimits(minLevel ?? 0, maxLevel ?? 4000);
+        }
     }
 }
