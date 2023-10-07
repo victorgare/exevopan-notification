6c8b1cf9936a178de5e51e2e9d55a069be50d429
Victor Rodrigues <garevictor@hotmail.com>
2023-10-07 18:43:45 +0100

    Add swagger docs for production

--- src/ExevopanNotification.Api/Program.cs
+++ src/ExevopanNotification.Api/Program.cs
@@ -34,12 +34,8 @@ public class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            // use custom swagger
-            app.UseCustomSwagger();
-        }
+        // use custom swagger
+        app.UseCustomSwagger();
 
         app.UseHttpsRedirection();
 
@@ -65,28 +61,4 @@ public class Program
         services.AddCustomSwagger();
     }
 }
-//var builder = WebApplication.CreateBuilder(args);
 
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
