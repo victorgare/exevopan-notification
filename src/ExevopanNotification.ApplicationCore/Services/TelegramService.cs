dc8b0229f51f9254b13cb9e13fdce809b96690fa
Victor Rodrigues <garevictor@hotmail.com>
2024-03-17 22:01:46 +0000

    Feat/improve rule breaker message (#14)
    * fix:deploy config
    
    * Added new listing option with cache
    
    * Improved message
    
    * Fixed group channels

--- src/ExevopanNotification.ApplicationCore/Services/TelegramService.cs
+++ src/ExevopanNotification.ApplicationCore/Services/TelegramService.cs
@@ -1,10 +1,12 @@
 ﻿using ExevopanNotification.ApplicationCore.Interfaces.Services;
 using ExevopanNotification.Domain.Config;
 using ExevopanNotification.Domain.Notifications;
+using ExevopanNotification.Utils.Utils;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using System.Text;
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 
 namespace ExevopanNotification.ApplicationCore.Services
 {
@@ -13,11 +15,14 @@ namespace ExevopanNotification.ApplicationCore.Services
         private readonly TelegramBotClient _telegramBotClient;
         private readonly TelegramConfig _telegramConfig;
         private readonly IMemoryCache _memoryCache;
+        private readonly IMemoryCache _memoryCache;
 
+        public TelegramService(IOptions<ApplicationConfig> appConfig, IMemoryCache memoryCache)
         public TelegramService(IOptions<ApplicationConfig> appConfig, IMemoryCache memoryCache)
         {
             _telegramConfig = appConfig.Value.TelegramConfig;
             _telegramBotClient = new TelegramBotClient(_telegramConfig.Key);
+            _memoryCache = memoryCache;
             _memoryCache = memoryCache;
         }
 
@@ -30,10 +35,19 @@ namespace ExevopanNotification.ApplicationCore.Services
                     chatId: _telegramConfig.GroupId,
                     text: telegramNotification.ToString(),
                     replyMarkup: telegramNotification.GetInlineLinkButton());
+                await _telegramBotClient.SendTextMessageAsync(
+                    chatId: _telegramConfig.GroupId,
+                    text: telegramNotification.ToString(),
+                    replyMarkup: telegramNotification.GetInlineLinkButton());
             }
         }
         public async Task NotifyRuleBreaker(List<AuctionNotification> auctionsNotifications)
         {
+            var newAuction = new StringBuilder();
+            var currentAuction = new StringBuilder();
+
+            var lastMessage = GetLastMessage<string[]>(_telegramConfig.RuleBreakerGroupId) ?? [];
+
             var newAuction = new StringBuilder();
             var currentAuction = new StringBuilder();
 
@@ -58,19 +72,17 @@ namespace ExevopanNotification.ApplicationCore.Services
             }
 
             var message = new StringBuilder()
-                .AppendLine("**NOVOS LEILOES**")
-                .AppendLine(newAuction.ToString())
-                .AppendLine("**FINALIZADOS**")
-                .AppendLine(string.Join(Environment.NewLine, lastMessage))
-                .AppendLine("**CONTINUAM**")
-                .AppendLine(currentAuction.ToString());
+                .Add("NOVOS LEILOES".ToBold(), newAuction)
+                .Add("FINALIZADOS".ToBold(), string.Join(Environment.NewLine, lastMessage))
+                .Add("CONTINUAM".ToBold(), currentAuction);
 
             SetLastMessage(_telegramConfig.RuleBreakerGroupId, auctionsNotifications.Select(c => c.Auction.Nickname).ToArray());
 
             await _telegramBotClient.SendTextMessageAsync(
                 chatId: _telegramConfig.RuleBreakerGroupId,
                 text: message.ToString(),
-                allowSendingWithoutReply: true);
+                allowSendingWithoutReply: true,
+                parseMode: ParseMode.Markdown);
         }
 
         public async Task Notify(string message)
@@ -98,6 +110,38 @@ namespace ExevopanNotification.ApplicationCore.Services
             }
 
             return default;
+        }
+
+
+    }
+
+    internal static class ExtensionHelper
+    {
+        internal static StringBuilder Add(this StringBuilder sb, string label, StringBuilder value)
+        {
+            if (value.Length > 0)
+            {
+                sb.AddLines(label, value.ToString());
+            }
+
+            return sb;
+        }
+
+        internal static StringBuilder Add(this StringBuilder sb, string label, string value)
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                sb.AddLines(label, value);
+            }
+
+            return sb;
+        }
+
+        private static StringBuilder AddLines(this StringBuilder sb, string label, string value)
+        {
+            return sb.AppendLine(label)
+                  .AppendLine(value.ToString())
+                  .AppendLine(Environment.NewLine);
         }
     }
 }
---
+++ src/ExevopanNotification.Utils/Utils/MarkdownExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace ExevopanNotification.Utils.Utils
+{
+    public static class MarkdownExtensions
+    {
+        public static string ToBold(this string value) => $"*{value}*";
+        public static string ToItalic(this string value) => $"_{value}_";
+
+    }
+}
