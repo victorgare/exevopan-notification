32660fb5452f8097d25911e52fd06802681dbf2f
Victor Rodrigues <garevictor@hotmail.com>
2024-08-19 15:08:01 +0100

    Added price per million xp

--- src/ExevopanNotification.Domain/Notifications/RuleBreakerNotification.cs
+++ src/ExevopanNotification.Domain/Notifications/RuleBreakerNotification.cs
@@ -14,8 +14,26 @@ namespace ExevopanNotification.Domain.Notifications
         public override string ToString()
         {
             var stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine($@" - {VocationIcon}{_auction.VocationId} [{_auction.Level}] - {_auction.Nickname}");
+            stringBuilder.AppendLine($@" - {VocationIcon}{_auction.VocationId} [{_auction.Level}] - {_auction.Nickname} - 💰({PricePerMillion}tc/kk)");
             return stringBuilder.ToString();
         }
+
+        private double PricePerMillion
+        {
+            get
+            {
+                var totalXp = TotalXp(_auction.Level);
+                var totalMillionsXp = Math.Round(totalXp / 1000000);
+                var result = (double)(_auction.CurrentBid / totalMillionsXp);
+                return double.Round(result, 2);
+            }
+        }
+
+        private static double TotalXp(int level)
+        {
+            return Math.Ceiling(50 * Math.Pow(level, 3) / 3 - 100 * Math.Pow(level, 2) + 850 * level / 3 - 200);
+
+        }
+
     }
 }
