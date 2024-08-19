a3711d31f6b0f59f490fbcc5d80dae663e3ec2bc
Victor Rodrigues <garevictor@hotmail.com>
2024-08-19 15:08:44 +0100

    feat: price per million xp

---
+++ src/ExevopanNotification.UnitTests/Domain/Notifications/RuleBreakerNotificationTests.cs
@@ -0,0 +1,28 @@
+﻿using ExevopanNotification.Domain.Entities;
+using ExevopanNotification.Domain.Notifications;
+using FluentAssertions;
+
+namespace ExevopanNotification.UnitTests.Domain.Notifications
+{
+    public class RuleBreakerNotificationTests
+    {
+        [Fact]
+        public void ShouldCalculatePricePerMillionOfXpCorrectly()
+        {
+            // arrange
+            var auction = new Auction
+            {
+                CurrentBid = 70251,
+                Level = 1020
+            };
+
+            var notication = new RuleBreakerNotification(auction);
+
+            // act
+            var result = notication.ToString();
+
+            // assert
+            result.Should().Contain("💰(4tc/kk)");
+        }
+    }
+}
